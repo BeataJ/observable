11ac1b820ca9af9a71ae2d114a13c54651d8c679
beataj <beata@emc2wave.com>
2021-07-31 16:43:06 -0700

    create interval Observable and subscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
+   const number = interval(1000);
+
+  //  const takeNumber = number.pipe(take(10))
+
+   this.firstSubscription = number.subscribe(x => console.log('Next: ', x))
+  }
+
+  ngOnDestroy(){
+    this.firstSubscription.unsubscribe();
   }
 
 }
