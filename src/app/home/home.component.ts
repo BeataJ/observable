efcc6a0f2d2a516fefcb46f524e9016eae1b1da8
beataj <beata@emc2wave.com>
2021-08-01 16:54:13 -0700

    add observer error complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,11 +23,20 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater 3!'))
+        }
         count++
       }, 1000)  
     });
 
-    this.firstSubscription = customObservable.subscribe(val => console.log(val));
+    this.firstSubscription = customObservable.subscribe(val => console.log(val), error => {
+      console.log(error);
+      alert(error.message)
+    });
   }
 
   ngOnDestroy(){
